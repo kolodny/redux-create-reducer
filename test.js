06abdb1c7f11902c0f72e9a5c538510631f48928
Moshe Kolodny <kolodny.github@gmail.com>
2015-09-07 20:02:15 -0400

    use var instead of const for tests

--- test.js
+++ test.js
@@ -4,36 +4,36 @@ var createReducer = require('./').createReducer;
 describe('createReducer', function() {
 
   it('returns the inital state on the first call with no matches', function() {
-    const reducerMap = {};
-    const reducer = createReducer('theintialstate', reducerMap);
+    var reducerMap = {};
+    var reducer = createReducer('theintialstate', reducerMap);
     expect(reducer(undefined, {type: 'YOLO'})).toEqual('theintialstate');
   });
 
   it('returns the correct state on the first call with a matche', function() {
-    const reducerMap = {
+    var reducerMap = {
       YOLO: function() {
         return 'theproperstate';
       }
     };
-    const reducer = createReducer('theintialstate', reducerMap);
+    var reducer = createReducer('theintialstate', reducerMap);
     expect(reducer(undefined, {type: 'YOLO'})).toEqual('theproperstate');
   });
 
   it('returns the same state if no value matched', function() {
-    const reducerMap = {};
-    const reducer = createReducer({someObj: true}, reducerMap);
-    const state = reducer(undefined, {type: 'YOLO'});
+    var reducerMap = {};
+    var reducer = createReducer({someObj: true}, reducerMap);
+    var state = reducer(undefined, {type: 'YOLO'});
     expect(reducer(state, {type: 'YOLO'})).toEqual(state);
   });
 
   it('returns a new state if a value matched', function() {
-    const reducerMap = {
+    var reducerMap = {
       YOLO: function() {
         return {someObj: 2};
       }
     };
-    const reducer = createReducer({someObj: 1}, reducerMap);
-    const state = reducer(undefined, {});
+    var reducer = createReducer({someObj: 1}, reducerMap);
+    var state = reducer(undefined, {});
     expect(reducer(state, {type: 'YOLO'})).toEqual({someObj: 2});
   });
 
